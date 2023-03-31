64f8e477abdb8cc792e0d7d0942b2e75a1b8e4db
rin-00 <2403670885@qq.com>
2023-03-31 19:43:28 +0800

    first commit by cn

---
+++ src/pages/exp8/Exp8.vue
@@ -0,0 +1,15 @@
+<!-- 8.软件项目/产品的财务评价实验
+包括: 净现值法、IRR 法、投资回收期法等 -->
+<template>
+
+
+
+</template>
+  
+  <script lang="ts">
+    export default {
+      name: 'Exp8',
+    };
+  </script>
+  
+  <style scoped lang="less"></style>
